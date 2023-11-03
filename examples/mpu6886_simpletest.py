c9fb12a0e41a22b8322c11a5e2a8ba3c1e46b6c0
Taiki Komoda <komoda@jins.com>
2023-11-03 16:14:45 +0900

    wip

--- examples/mpu6886_simpletest.py
+++ examples/mpu6886_simpletest.py
@@ -15,8 +15,10 @@ i2c = I2C(board.IMU_SCL, board.IMU_SDA)
 mpu = mpu6886.MPU6886(i2c)
 
 while True:
-    print(f"Acceleration: X:{mpu.acceleration[0]:.2f}, Y:{mpu.acceleration[1]:.2f}, Z:{mpu.acceleration[2]:.2f} m/s^2")
-    print(f"Gyro X:{mpu.gyro[0]:.2f}, Y:{mpu.gyro[1]:.2f}, Z:{mpu.gyro[2]:.2f} degrees/s")
+    ma = mpu.acceleration
+    mg = mpu.gyro
+    print(f"Acceleration: X:{ma[0]:.2f}, Y:{ma[1]:.2f}, Z:{ma[2]:.2f} m/s^2")
+    print(f"Gyro X:{mg[0]:.2f}, Y:{mg[1]:.2f}, Z:{mg[2]:.2f} degrees/s")
     print(f"Temperature: {mpu.temperature:.2f} C")
     print("")
     time.sleep(1)
